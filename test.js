e38062b803c3b440ba2e7e19569c1254310d1308
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 11:07:31 -0400

    run update, lint, `verb --init`

--- test.js
+++ test.js
@@ -1,29 +1,22 @@
-/*!
- * for-own <https://github.com/jonschlinkert/for-own>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
-
 'use strict';
 
-/* deps:mocha */
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var forOwn = require('./');
 
-describe('.forOwn()', function () {
-  it('should loop through all properties in the object.', function () {
+describe('.forOwn()', function() {
+  it('should loop through all properties in the object.', function() {
     var obj = {a: 'foo', b: 'bar', c: 'baz'};
     var values = [];
     var keys = [];
 
-    forOwn(obj, function (value, key, o) {
-      o.should.eql(obj);
+    forOwn(obj, function(value, key, o) {
+      assert.deepEqual(o, obj);
       keys.push(key);
       values.push(value);
     });
 
-    keys.should.eql(['a', 'b', 'c']);
-    values.should.eql(['foo', 'bar', 'baz']);
+    assert.deepEqual(keys, ['a', 'b', 'c']);
+    assert.deepEqual(values, ['foo', 'bar', 'baz']);
   });
 });
